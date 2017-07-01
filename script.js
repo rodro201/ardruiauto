e9f3f2e2ff67a5e16d69e3e375246102ac79623c
rodro201 <rodrigoacchini@hotmail.com>
2017-07-01 11:14:53 -0300

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,25 @@
+	$('.arriba').mousedown(function() {
+		$('.arriba').toggleClass('click')
+	});
+	$('.arriba').mouseup(function() {
+		$('.arriba').removeClass('click')
+	});
+	$('.abajo').mousedown(function() {
+		$('.abajo').toggleClass('click')
+	});
+	$('.abajo').mouseup(function() {
+		$('.abajo').removeClass('click')
+	});
+	$('.derecha').mousedown(function() {
+		$('.derecha').toggleClass('click')
+	});
+	$('.derecha').mouseup(function() {
+		$('.derecha').removeClass('click')
+	});
+	$('.izquierda').mousedown(function() {
+		$('.izquierda').toggleClass('click')
+	});
+	$('.izquierda').mouseup(function() {
+		$('.izquierda').removeClass('click')
+	});
+	
